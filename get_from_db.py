3e61912f7dd2a7c1d23f3e9936fb99638e6c1a29
SabineHaas <sabine.haas@rl-institut.de>
2017-07-31 14:31:31 +0200

    Make calculate_calms() faster

--- get_from_db.py
+++ get_from_db.py
@@ -88,16 +88,17 @@ def calculate_calms(calms_dict):
     """
     calms_max, calms_min, calm_lengths = {}, {}, {}
     for key in calms_dict:
+        df = calms_dict[key]
         # Find calm periods
-        calms, = np.where(calms_dict[key]['calm'] != 'no_calm')
+        calms, = np.where(df['calm'] != 'no_calm')
         calm_arrays = np.split(calms, np.where(np.diff(calms) != 1)[0] + 1)
         # Write the calm lengths into array of dictionary calm_lengths
         calm_lengths[key] = np.array([len(calm_arrays[i])
                                      for i in range(len(calm_arrays))])
         # Find the longest and shortest calm from all periods
-        maximum = max(calm_lengths[key])
+        maximum = len(max(calm_arrays, key=len))
         calms_max[key] = maximum
-        minimum = min(calm_lengths[key])
+        minimum = len(min(calm_arrays, key=len))
         calms_min[key] = minimum
     # Create DataFrame
     calms_max = pd.DataFrame(data=calms_max, index=['results']).transpose()
